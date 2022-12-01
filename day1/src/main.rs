42ecdea387d27e6efd85664e7868f5a86ab00985
Matt Corbin <mcorbin93@gmail.com>
2022-12-01 00:10:10 -0500

    day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,11 +1,34 @@
 use std::fs;
 
+
+fn parse_input(input: &str) -> Vec<Vec<usize>> {
+    let mut retval = Vec::new();
+    let mut current = Vec::new();
+    for line in input.lines() {
+        if line.is_empty() {
+            retval.push(current);
+            current = Vec::new();
+        } else {
+            let calories = line.parse::<usize>().expect("should be a number");
+            current.push(calories);
+        }
+    }
+    retval
+}
+
 fn part1(input: &str) {
-    println!("part1: {}", 0)
+    let elves = parse_input(input);
+    let max_calories: usize = elves.iter().map(|x| x.iter().sum()).max().unwrap();
+    println!("part1: {}", max_calories)
 }
 
 fn part2(input: &str) {
-    println!("part2: {}", 0)
+    let elves = parse_input(input);
+    let mut elves = elves.iter().map(|x| x.iter().sum()).collect::<Vec<usize>>();
+    elves.sort();
+    elves.reverse();
+
+    println!("part2: {}", elves[0] + elves[1] + elves[2])
 }
 
 fn main() {
