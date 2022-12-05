f826e72e2c3bcec706e9a12e089600080e938ea0
Matt Corbin <mcorbin93@gmail.com>
2022-12-05 00:24:34 -0500

    day 5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,11 +1,81 @@
 use std::fs;
 
+struct Stacks {
+    items: Vec<Vec<char>>,
+}
+
+struct Instruction {
+    count: usize,
+    src: usize,
+    dst: usize,
+}
+
+fn bootstrap_stacks() -> Stacks {
+    Stacks {
+        items: vec![] // Redacted
+    }
+}
+
+fn parse_instructions(input: &str) -> Vec<Instruction> {
+    let mut retval = Vec::new();
+    for line in input.lines() {
+        let mut splits = line.strip_prefix("move ").unwrap().split(" from ");
+        let count = splits.next().unwrap().parse().unwrap();
+        let mut stacks = splits.next().unwrap().split(" to ");
+        let src = stacks.next().unwrap().parse().unwrap();
+        let dst = stacks.next().unwrap().parse().unwrap();
+        retval.push(
+          Instruction{
+              count,
+              src,
+              dst,
+          }  
+        );
+    }
+    retval
+}
+
 fn part1(input: &str) {
-    println!("part1: {}", 0)
+    let mut stacks = bootstrap_stacks();
+    let instructions = parse_instructions(input);
+    for instruction in instructions {
+        for _ in 0..instruction.count {
+            let mv = stacks.items[instruction.src-1].pop().unwrap();
+            stacks.items[instruction.dst-1].push(mv);
+        }
+    }
+    let mut output = String::new();
+    for mut stack in stacks.items {
+        if let Some(top) = stack.pop() {
+            output.push(top);
+        }
+
+    }
+    println!("part1: {}", output)
 }
 
 fn part2(input: &str) {
-    println!("part2: {}", 0)
+    let mut stacks = bootstrap_stacks();
+    let instructions = parse_instructions(input);
+    for instruction in instructions {
+        let mut temp = Vec::new();
+        for _ in 0..instruction.count {
+            let mv = stacks.items[instruction.src-1].pop().unwrap();
+            temp.push(mv);
+        }
+        temp.reverse();
+        for item in temp {
+            stacks.items[instruction.dst-1].push(item);
+        }
+    }
+    let mut output = String::new();
+    for mut stack in stacks.items {
+        if let Some(top) = stack.pop() {
+            output.push(top);
+        }
+
+    }
+    println!("part2: {}", output)
 }
 
 fn main() {
